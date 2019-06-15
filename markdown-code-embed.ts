da8e4b030cc54b1744a3c0b41e2182c41de99970
Zak Henry <zak@cloudnc.com>
2019-06-15 17:18:39 +0100

    refactor(Readme Builder): Rewrite builder to allow for editing in-place the readme

--- markdown-code-embed.ts
+++ markdown-code-embed.ts
@@ -7,31 +7,43 @@ import { extname } from 'path';
  *
  * Example:
  *
- * ```path/to/some/file.ts
+ * ```ts
+ * // path/to/some/file.ts
  * ```
  *
  * will look for path/to/some/file.ts and if present, read it and insert
  *
  * ```ts
- * // file content will appear hear
- * ```
- *
- * ```path/to/some/file.ts#L10-50
+ * // path/to/some/file.ts
+ * file content will appear hear
  * ```
  *
  * ```ts
- * // file content (only lines 10 - 50) will appear hear
+ * // path/to/some/file.ts#L10-50
  * ```
  *
+ * ```ts
+ * // path/to/some/file.ts#L10-50
+ * file content (only lines 10 - 50) will appear hear
+ * ```
  *
  */
 
-const [proc, thisFile, source, outputFile] = process.argv;
+const [proc, thisFile, source] = process.argv;
 
 const sourceText = readFileSync(source, 'utf-8');
 
-const output = sourceText.replace(/```([\S]*)$\n([\s\S]*?)\n?```/gm, (substr: string, codeExtension: string) => {
-  const matches = codeExtension.match(/\s?(\S+?)((#L(\d+)-L(\d+))|$)/m);
+/**
+ * Match a codefence, capture groups around the file extension (optional) and first line starting with // (optional)
+ */
+const codeFenceMatcher: RegExp = /```([\S]*)$\n(\/\/[\s\S]*?$)?([\s\S]*?)\n?```/gm;
+
+const output = sourceText.replace(codeFenceMatcher, (substr: string, codeExtension: string, firstLine?: string) => {
+  if (!firstLine) {
+    return substr;
+  }
+
+  const matches = firstLine.match(/\s?(\S+?)((#L(\d+)-L(\d+))|$)/m);
 
   if (!matches) {
     return substr;
@@ -46,18 +58,35 @@ const output = sourceText.replace(/```([\S]*)$\n([\s\S]*?)\n?```/gm, (substr: st
   const extension = extname(filename).slice(1);
   const file = readFileSync(filename, 'utf8');
 
-  let outputCode = file;
-
+  let lines = file.split('\n');
   if (lineNumbering) {
-    const lines = file.split('\n');
-
-    outputCode = lines.slice(+startLine - 1, +endLine).join('\n');
+    lines = lines.slice(+startLine - 1, +endLine);
   }
 
+  const minimumLeadingSpaces = lines.reduce((minSpaces, line) => {
+    if (minSpaces === 0) {
+      return;
+    }
+
+    const leadingSpaces = line.match(/^[\s]+/m);
+
+    if (!leadingSpaces) {
+      return 0;
+    }
+
+    return Math.min(minSpaces, leadingSpaces[0].length);
+  }, Infinity);
+
+  lines = lines.map(line => line.slice(minimumLeadingSpaces));
+
+  const outputCode = lines.join('\n');
+
   return `\`\`\`${extension}
-${outputCode.trim()}
+${firstLine}
+
+${outputCode}
 \`\`\``;
 });
 
-console.log(output);
-writeFileSync(outputFile, output);
+// console.log(output);
+writeFileSync(source, output);
