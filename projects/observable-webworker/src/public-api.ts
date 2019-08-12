641af4f835cfa75553aaa6fba4a20a9f9992c784
Zak Henry <zak@cloudnc.com>
2019-08-12 01:22:58 +0100

    feat(concurrentConcatAll): Add custom operator for managing multiple  concurrent inner subscriptions but maintaining the output order

--- projects/observable-webworker/src/public-api.ts
+++ projects/observable-webworker/src/public-api.ts
@@ -7,3 +7,5 @@ export * from './lib/observable-worker.decorator';
 export * from './lib/run-worker';
 export * from './lib/from-worker';
 export * from './lib/from-worker-pool';
+// @todo move down to a 'observable-worker/helper' sub module to ensure it is not always bundled
+export * from './lib/concurrent-concat-all';
