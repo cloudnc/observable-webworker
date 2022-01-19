7abd4a85c5431b17107bef746e950272411fefa5
Maxime Robert <maxime.robert1992@gmail.com>
2022-01-19 15:27:00 +0100

    feat: upgrade to angular 12

--- src/readme/transferable.main.ts
+++ src/readme/transferable.main.ts
@@ -5,7 +5,7 @@ export function computeHash(arrayBuffer: ArrayBuffer): Observable<string> {
   const input$ = of(arrayBuffer);
 
   return fromWorker<ArrayBuffer, string>(
-    () => new Worker('./transferable.worker', { type: 'module' }),
+    () => new Worker(new URL('./transferable.worker', import.meta.url), { type: 'module' }),
     input$,
     input => [input],
   );
